dc959d3d06808c7e4623f4b54d0551c237494529
Anton K. <antonkarlovskiy@outlook.com>
2025-11-06 09:50:08 -0500

    refactor: configure multiple workers

--- src/hooks/useWorker.ts
+++ src/hooks/useWorker.ts
@@ -1,20 +1,34 @@
 import { useState } from "react";
 
 export interface MessageEventHandler {
-    (event: MessageEvent): void;
+  (event: MessageEvent): void;
 }
 
-export function useWorker(messageEventHandler: MessageEventHandler): Worker {
-    // Create new worker once and never again
-    const [worker] = useState(() => createWorker(messageEventHandler));
-    return worker;
+export type WorkerType = "transcription" | "summarization";
+
+export function useWorker(workerType: WorkerType, messageEventHandler: MessageEventHandler): Worker {
+  // Create new worker once and never again
+  const [worker] = useState(() => createWorker(workerType, messageEventHandler));
+  return worker;
 }
 
-function createWorker(messageEventHandler: MessageEventHandler): Worker {
-    const worker = new Worker(new URL("../transcription-worker.js", import.meta.url), {
-        type: "module",
-    });
-    // Listen for messages from the Web Worker
-    worker.addEventListener("message", messageEventHandler);
-    return worker;
+function createWorker(workerType: WorkerType, messageEventHandler: MessageEventHandler): Worker {
+  let worker: Worker;
+  switch (workerType) {
+    case "transcription":
+      worker = new Worker(new URL("../workers/transcription-worker.js", import.meta.url), {
+          type: "module",
+      });
+      break;
+    case "summarization":
+      worker = new Worker(new URL("../workers/summarization-worker.js", import.meta.url), {
+          type: "module",
+      });
+      break;
+    default:
+      throw new Error(`Unknown worker type: ${workerType}`);
+  }
+  // Listen for messages from the Web Worker
+  worker.addEventListener("message", messageEventHandler);
+  return worker;
 }
